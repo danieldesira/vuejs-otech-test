af45f469952c4e00ce1be68808b6b50bb747a486
Daniel Desira <desiradaniel2007@gmail.com>
2025-11-17 06:15:51 +0100

    keep menu on top and increase font and height

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header class="flex bg-slate-500 max-h-12 items-center min-w-full px-5">
+  <header class="flex bg-slate-500 h-16 items-center min-w-full px-5">
     <div class="flex gap-5 w-full">
-      <nav class="flex gap-2 text-[12px] font-semibold">
+      <nav class="flex gap-2 text-[24px] font-semibold">
         <RouterLink to="/" class="focus:bg-slate-950 rounded-sm">Tasks</RouterLink>
         <RouterLink to="/about" class="focus:bg-slate-950 rounded-sm">About</RouterLink>
       </nav>
